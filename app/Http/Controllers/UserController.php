4a7287e31afc25eb91afa23744998cc242ed775a
Rinkuu253 <cemhyp123@gmail.com>
2022-07-02 21:11:28 +0700

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use DB;
 
 class UserController extends Controller
 {
     //
-    public function index()
+    public function getlist()
     {
-        return User::all();
+        return response()->json([
+            'success'=>'succes', 
+            'results'=>User::all()
+        ]);
+    }
+
+    public function login(request $request)
+    {
+        return $request;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('profile/getlist', [UserController::class, 'index']);
+Route::get('profile/getlist', [UserController::class, 'getlist']);
+
+Route::get('profile/login', [UserController::class, 'login']);
