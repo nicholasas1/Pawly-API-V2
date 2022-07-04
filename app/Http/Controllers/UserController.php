93595e9303be8f4359a7cb09ff70059752115f54
Rinkuu253 <cemhyp123@gmail.com>
2022-07-04 11:55:30 +0700

    M|Fixing Login role and user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Role;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -20,14 +20,14 @@ class UserController extends Controller
 
     public function login(request $request)
     {
-        
+
         return response()->json([
             'success'=>'succes', 
             'results'=> array(
-                'role' => User::where('username', '=', $request->Username)->orWhere('email', '=', $request->Username) ->Where('password', '=', $request->Password)->get(['id','username']),
-                'user' => User::where('username', '=', $request->Username)->orWhere('email', '=', $request->Username) ->Where('password', '=', $request->Password)->get(['id','username'])
+                'role' => User::where('username',$request->username)->orWhere('email',$request->username)->where('password',md5($request->password))->get(['id','username']),
+                'user' => User::where('username',$request->username)->orWhere('email',$request->username)->where('password',md5($request->password))->get(['id','username'])
             )
         ]);
-       
+
     }
 }
