d98c5bc52e37efa5659031c7be3aeadb55d2cf57
Rinkuu253 <cemhyp123@gmail.com>
2022-08-04 07:56:50 +0700

    m|add token and profile picture for pets

--- database/migrations/2022_07_12_041317_create_userpets_table.php
+++ database/migrations/2022_07_12_041317_create_userpets_table.php
@@ -16,20 +16,22 @@ return new class extends Migration
         Schema::create('userpets', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('user_id',100);
+            $table->text('token',100);
             $table->text('petsname',100);
+            $table->text('pets_picture',100)->nullable();
             $table->text('species',100);
             $table->text('breed',100);
             $table->text('size',100)->nullable();
             $table->text('gender',10);
             $table->date('birthdate');
-            $table->boolean('neutered')->nullable();
-            $table->boolean('vaccinated')->nullable();
-            $table->boolean('fdlwdogs')->nullable(); //friendly with dogs
-            $table->boolean('fdlwcats')->nullable(); //friendly with cats
-            $table->boolean('fdlywkidsless10')->nullable(); //friendly with kids < 10 years old
-            $table->boolean('fdlwkidsmore10')->nullable(); //friendly with kids > 10 years old
-            $table->boolean('microchipped')->nullable();
-            $table->boolean('purbered')->nullable();
+            $table->text('neutered',5)->nullable();
+            $table->text('vaccinated',5)->nullable();
+            $table->text('fdlwdogs',5)->nullable(); //friendly with dogs
+            $table->text('fdlwcats',5)->nullable(); //friendly with cats
+            $table->text('fdlywkidsless10',5)->nullable(); //friendly with kids < 10 years old
+            $table->text('fdlwkidsmore10',5)->nullable(); //friendly with kids > 10 years old
+            $table->text('microchipped',5)->nullable();
+            $table->text('purbered',5)->nullable();
         });
     }
 
