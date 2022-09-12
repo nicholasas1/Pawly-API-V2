9b90ffcb4643323c5bd09c65fd7d2aa04acf1d7a
nicholasas1 <nicholasantonius46@gmail.com>
2022-09-12 13:37:29 +0700

    Create uniqe and random secret

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ClinicController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\JWTValidator;
 use App\Http\Controllers\RatingsController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\SplashscreenMobileController;
@@ -88,6 +89,8 @@ Route::post('doctor/lastonline', [DoctorController::class, 'lastonline']);
 Route::get('doctor/filtersearch', [DoctorController::class, 'filtersearch']);
 
 Route::post('ratings/add', [RatingsController::class, 'addratings']);
+
+Route::get('validateToken', [JWTValidator::class, 'validateToken']);
 
 
 
