5d8fa0e44bd8aa6f0b70c2ce84c241c9f6e437d8
nicholasas1 <nicholasantonius46@gmail.com>
2022-09-16 13:50:29 +0700

    Hot Fix Rating Service

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -6,18 +6,34 @@ use App\Models\ratings;
 use App\Models\doctor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\JWTValidator;
+use App\Models\clinic;
 
 class RatingsController extends Controller
 {
-   public function addratings(request $request){
 
+    public function __construct(JWTValidator $JWTValidator)
+    {
+        $this->JWTValidator = $JWTValidator;
+    }
+
+    public function addratings(request $request){
+        if($request->header("Authorization") == null){
+            $token = "123456.dfdsxcfd.45gdcsxsd";
+        }else{
+           $token = $request->header("Authorization");
+        }
+        
+        $result = $this->JWTValidator->validateToken($token);
         $status = 'doctor not found';
         $doctortrue = doctor::where('id',$request->doctor_id);
-
+        //$doctortrue = clinic::where('id',$request->clinic_id);
+        $user = $result['body']['user_id'];
         if($doctortrue->count()>0){
             $query = DB::table('ratings')->insert([
                 'doctors_ids' => $request->doctor_id,
-                'users_id' => $request->user_id,
+                //'clinic_id' => $request->clinic_id,
+                'users_id' => $user,
                 'ratings' => $request->rating,
                 'reviews' => $request->reviews
             ]);
@@ -33,5 +49,5 @@ class RatingsController extends Controller
             
         }
        
-   }
+    }
 }
