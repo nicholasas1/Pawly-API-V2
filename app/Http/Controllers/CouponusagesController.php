df82ccf4d2a3bb49dda4bb8d7ae37d6134b44a0b
Rinkuu253 <cemhyp123@gmail.com>
2022-09-27 07:28:28 +0700

    m|add table and model for order and coupon

---
+++ app/Http/Controllers/CouponusagesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\couponusages;
+use Illuminate\Http\Request;
+
+class CouponusagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\couponusages  $couponusages
+     * @return \Illuminate\Http\Response
+     */
+    public function show(couponusages $couponusages)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\couponusages  $couponusages
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(couponusages $couponusages)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\couponusages  $couponusages
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, couponusages $couponusages)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\couponusages  $couponusages
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(couponusages $couponusages)
+    {
+        //
+    }
+}
