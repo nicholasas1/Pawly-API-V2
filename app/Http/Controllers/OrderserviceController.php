a1269c8f643e95df5e7818aef3d2d2e6f2c0f425
Rinkuu253 <cemhyp123@gmail.com>
2022-09-30 06:57:19 +0700

    m|add coupon servce not done

--- app/Http/Controllers/OrderserviceController.php
+++ app/Http/Controllers/OrderserviceController.php
@@ -7,79 +7,9 @@ use Illuminate\Http\Request;
 
 class OrderserviceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\orderservice  $orderservice
-     * @return \Illuminate\Http\Response
-     */
-    public function show(orderservice $orderservice)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\orderservice  $orderservice
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(orderservice $orderservice)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\orderservice  $orderservice
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, orderservice $orderservice)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\orderservice  $orderservice
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(orderservice $orderservice)
-    {
-        //
+    public function order_detail($service,$price,$coupon_name){
+        if($coupon_name==NULL){
+            
+        }
     }
 }
