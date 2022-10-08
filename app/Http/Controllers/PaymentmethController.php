72a15d3be8e779ba130d4dfb09d404f90b76584e
Nicholas Antonius <nicholas@strongbee.co.id>
2022-10-08 12:46:24 +0700

    array push payment method

--- app/Http/Controllers/PaymentmethController.php
+++ app/Http/Controllers/PaymentmethController.php
@@ -8,20 +8,49 @@ use stdClass;
 
 class PaymentmethController extends Controller
 {
-    public function payment_method($service,$payment){
-        $arr = ['ovo','gopay','wallet'];
+    //$service,$payment
+    public function payment_method(Request $request){
+        $arr = array(
+            array('payment_method' => 'Ovo',
+            'image_url'=>'http://',
+            'description'=>'make your life more simple'),
+
+            array('payment_method' => 'Dana',
+            'image_url'=>'http://',
+            'description'=>'make your life more simple'),
+
+            array('payment_method' => 'GoPay',
+            'image_url'=>'http://',
+            'description'=>'make your life more simple'),
+
+        );
+      
         $payment_allowed = new stdClass;
 
-        if($payment==NULL){
-            $allowed_payment = paymentmeth::where('service',$service)->select('allowed_payment')->get();
-            foreach($allowed_payment as $allow){
-                if(array_key_exists($allow, $arr)){
-                    $payment_allowed->payment = $allow;
-                    $payment_allowed->access = 'true';
-                } else{
-                    $payment_allowed->payment = $allow;
-                    $payment_allowed->access = 'false';
+        if($request->payment==NULL){
+            $allowed_payment = unserialize(paymentmeth::where('service',$request->service)->value('allowed_payment'));
+            $payment_allowed = array();
+            
+            foreach($arr as $arr){
+                foreach($allowed_payment as $allow){
+                    if($arr['payment_method'] == $allow){
+                        $method = array(
+                            'payment_method' => $arr['payment_method'],
+                            'image_url'=>$arr['image_url'],
+                            'description'=>$arr['description'],
+                            'active'=>true
+                        );
+                    }else{
+                        $method = array(
+                            'payment_method' => $arr['payment_method'],
+                            'image_url'=>$arr['image_url'],
+                            'description'=>$arr['description'],
+                            'active'=>false
+                        );
+                    }
+                    array_push($payment_allowed, $method);
                 }
+             
             }
             return response()->JSON([
                 'status' => 'success',
--- database/migrations/2022_09_29_062822_create_paymentmeths_table.php
+++ database/migrations/2022_09_29_062822_create_paymentmeths_table.php
@@ -16,11 +16,7 @@ return new class extends Migration
         Schema::create('paymentmeths', function (Blueprint $table) {
             $table->id();
             $table->char('service',20)->nullable();
-            $table->char('allowed_payment',20)->nullable();
-            $table->date('payment_at')->nullable();
-            $table->float('payment_amount',20,2)->nullable();
-            $table->char('payment_use',20)->nullable();
-            $table->char('payment_id',20)->nullable();
+            $table->char('allowed_payment',200)->nullable();
         });
     }
 
