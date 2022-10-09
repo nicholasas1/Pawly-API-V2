e1b24272999fcacb61091556825d8a379b600347
Rinkuu253 <cemhyp123@gmail.com>
2022-10-09 17:02:48 +0700

    m|order service use token

--- app/Http/Controllers/OrderserviceController.php
+++ app/Http/Controllers/OrderserviceController.php
@@ -5,24 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\orderservice;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CouponserviceController;
+use ReallySimpleJWT\Token;
+use ReallySimpleJWT\Parse;
+use ReallySimpleJWT\Jwt;
+use ReallySimpleJWT\Decode;
+use App\Http\Controllers\JWTValidator;
 
 class OrderserviceController extends Controller
 {
     protected $coupons;
-
-    public function __construct(CouponserviceController $coupons)
+    protected $JWTValidator;
+    public function __construct(CouponserviceController $coupons, JWTValidator $jWTValidator)
     {
         $this->coupons = $coupons;
+        $this->JWTValidator = $jWTValidator;
     }
 
     public function order_service(request $request){
+
+        $token = $request->header("Authorization");
+        $result = $this->JWTValidator->validateToken($token);
 
         $service = $request->service;
         $price = $request->price;
         $coupon_name = $request->coupon;
         $service_id = $request->servid;
         $type = $request->type;
-        $userid = $request->id;
+        $userid = $result['body']['user_id'];
 
         $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
@@ -88,33 +97,11 @@ class OrderserviceController extends Controller
                 ]);
             }
             } else{
-                $total_price = $price;
-                $discount = $coupons_respond['value'];
-                $subtotal = $total_price-$discount;
-                $query = orderservice::insertGetId([
-                'service' => $service,
-                'service_id' => $service_id,
-                'type' => $type,
-                'status' => 'pending',
-                'users_ids' => $userid,
-                'total' => $total_price,
-                'diskon' => $discount,
-                'subtotal' => $subtotal
-            ]);
-            $insertorderid = orderservice::where('id',$query)->update([
-                'order_id' => substr(str_shuffle(str_repeat($pool, 5)), 0, 8).$query
-            ]);
-
-            if($insertorderid==1){
-                return response()->JSON([
-                    'status' => 'success',
-                    'results' => orderservice::where('id',$query)->get()
-                ]);
-            } else{
+                
                 return response()->JSON([
                     'status' => 'error'
                 ]);
-            }
+            
             }
         }
     }
