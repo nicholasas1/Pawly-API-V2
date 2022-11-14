71efb6372e17f07565994d504279c7bb601b11b3
Rinkuu253 <cemhyp123@gmail.com>
2022-11-14 18:11:39 +0700

    m|add table

---
+++ app/Http/Controllers/PenangananController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penanganan;
+use Illuminate\Http\Request;
+
+class PenangananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Penanganan  $penanganan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Penanganan $penanganan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Penanganan  $penanganan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Penanganan $penanganan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Penanganan  $penanganan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Penanganan $penanganan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Penanganan  $penanganan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Penanganan $penanganan)
+    {
+        //
+    }
+}
