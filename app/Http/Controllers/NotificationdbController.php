a658aa3888f60c763b091f46ed139c6090c9f3aa
Rinkuu253 <cemhyp123@gmail.com>
2022-11-19 15:03:39 +0700

    m|add notificationdb table

---
+++ app/Http/Controllers/NotificationdbController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\notificationdb;
+use Illuminate\Http\Request;
+
+class NotificationdbController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\notificationdb  $notificationdb
+     * @return \Illuminate\Http\Response
+     */
+    public function show(notificationdb $notificationdb)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\notificationdb  $notificationdb
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(notificationdb $notificationdb)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\notificationdb  $notificationdb
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, notificationdb $notificationdb)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\notificationdb  $notificationdb
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(notificationdb $notificationdb)
+    {
+        //
+    }
+}
