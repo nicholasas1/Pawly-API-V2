0ccbcde968d151293566b1ef84b85bf6e4f8f82a
Nicholas Antonius <nicholas@strongbee.co.id>
2023-02-01 17:32:21 +0700

    Bug Fix, service detail wa nnotifiction

--- app/Http/Controllers/OrderserviceController.php
+++ app/Http/Controllers/OrderserviceController.php
@@ -754,7 +754,11 @@ class OrderserviceController extends Controller
                             'partnerDetail' => $orderDetail['results']['partner_detail'],
                         ];
                         $this->mailServer->InvoicePaymentSuccessCusttomer($details);
-                        $chat = "Hallo, ".$details['partnerDetail']['name']." , mau info Ada bookingan masuk dari PAWLY APP:\n\nNama : ".$details['user_detail']['nickname']."\nBooking Service : ".$details['type']." - ".$details['service']."\nBooking Code : ".$details['order_id']."\n\nMohon dibantu proses ya kak, Terimakasih 🙏😊";
+                        $serviceData=[];
+                        foreach($details['service'] as $service){ 
+                            array_push($serviceData,$service['service_name']);
+                        };
+                        $chat = "Hallo, ".$details['partnerDetail']['name']." , mau info Ada bookingan masuk dari PAWLY APP:\n\nNama : ".$details['user_detail']['nickname']."\nBooking Service : ".$details['type']." - ". implode(" ",$serviceData)  ."\nBooking Code : ".$details['order_id']."\n\nMohon dibantu proses ya kak, Terimakasih 🙏😊";
 
                         $wa = $this->whatsapp->sendWaText($details['partnerDetail']['phone_number'], $chat);
                         $this->socket->update_order($orderId, $orderDetail['results']['status'],$orderDetail['results']['users_ids'],$orderDetail['results']['partner_user_id']);
